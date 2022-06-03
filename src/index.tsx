79dffbd80b02955ef41bdf87b7c28a0b7bae8d11
ZèD <imzihad@gmail.com>
2022-06-03 06:21:10 +0000

    Imported changes required for github pages
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tvmaze">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
